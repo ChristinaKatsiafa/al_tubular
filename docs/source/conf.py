33fe497148d95ee68e922b29c3c555fc26620819
Christina Katsiafa <137049408+ChristinaKatsiafa@users.noreply.github.com>
2025-04-07 13:03:34 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../../"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -33,6 +33,7 @@ extensions = [
 ]
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
 templates_path = ["_templates"]
 exclude_patterns = []
 
